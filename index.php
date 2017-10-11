625206c60a7bf6e936bd113d6f28da8e8ed38e66
Meyer <mjmeyer@ny1-lm002-p5365.aetvn.com>
2017-10-11 17:38:20 -0400

    Home updated, necesssities noted

--- index.php
+++ index.php
@@ -24,20 +24,14 @@
 
 		$connections = mysqli_query($connection, "SELECT * FROM friends WHERE user1='".$_SESSION['member_id']."' OR user2='".$_SESSION['member_id']."'");
 
+		echo '<h1 style="text-align:center;">You Have '.mysqli_num_rows($connections).' Connection(s)</h1>';
 
-    $allmembers = mysqli_query($connection, "SELECT * FROM members WHERE id != '".$_SESSION['member_id']."' ");
-    $rsallmembers = mysqli_fetch_array($allmembers);
-
-		echo '<h1>'.mysqli_num_rows($connections).' Connections</h1>';
-
-		// $connectionsList = '';
 		while( $rsConnections = mysqli_fetch_array($connections) ){
 
-      $allmembers = mysqli_query($connection, "SELECT * FROM members WHERE id='".$rsConnections['user1']."' OR id='".$rsConnections['user1']."'");
+      $allmembers = mysqli_query($connection, "SELECT * FROM members WHERE (id='".$rsConnections['user1']."' OR id='".$rsConnections['user2']."') AND id!='".$_SESSION['member_id']."'");
       $rsallmembers = mysqli_fetch_array($allmembers);
 
-
-			$userPInfo = mysqli_query($connection, "SELECT * FROM profile WHERE user_id='".$rsConnections['user1']."' OR user_id='".$rsConnections['user2']."'");
+			$userPInfo = mysqli_query($connection, "SELECT * FROM profile WHERE user_id='".$rsallmembers['id']."' AND user_id!='".$_SESSION['member_id']."'");
 
 			$rsuserPInfo = mysqli_fetch_array($userPInfo);
 
@@ -58,27 +52,27 @@
 			}
 
 
-			$MembersList .= '<div class="memberBox">
+			$MembersList .= '<div class="memberBox" style="padding:20px; border: 1px solid red;">
+          								<div class="" >
+          									'.$dp.'
+          								</div>
+                          <div style="border: 2px solid yellow; text-align: center;">
+            								<h3>
+            									<a href="user.php?name='.$rsallmembers['name'].'">'.ucfirst($rsallmembers['name']).'</a>
+            								</h3><br>
+                            <h4>
+                              <i>'. ((isset($rsCountryName['country_name']))?'<br>from '.$rsCountryName['country_name']:'') .'</i>
+                            </h4>
+                          </div>
+                        </div> <br>';
+    }
 
-								<div class="">
-									'.$dp.'
-								</div>
-								<h3 class="">
-									<a href="user.php?name='.$rsallmembers['name'].'">'.ucfirst($rsallmembers['name']).'</a>
-								</h3>
-                <h4>
-                  <i>'. ((isset($rsCountryName['country_name']))?'<br>from '.$rsCountryName['country_name']:'') .'</i>
-                </h4>
-
-
-							</div> <br>';
-
-		}
-
-		echo $MembersList;
+		// echo $MembersList;
 
 	?>
 
-
+<div class="connectionsContainer" style="padding: 10px; width: 1000px; height: auto; border: 1px solid green; display: flex; justify-content: center; flex-wrap: wrap;">
+  <?php echo $MembersList; ?>
+</div>
 
 <?php include('footer.php'); ?>
